b64840528dc3060af2225728ddd0bc6ab7301b2e
PROG2-P1 <PROG2-P1-prog2@enetcom.usf.tn>
2024-11-04 15:07:19 +0100

    init android project

---
+++ app/src/main/java/com/enetcom/roomwordsample/data/db/WordDao.java
@@ -0,0 +1,28 @@
+package com.enetcom.roomwordsample.data.db;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.enetcom.roomwordsample.model.Word;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+}
---
+++ app/src/main/java/com/enetcom/roomwordsample/model/Word.java
@@ -0,0 +1,27 @@
+package com.enetcom.roomwordsample.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    private String word;
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+
+    public String getWord(){return this.mWord;}
+
+
+}
+
